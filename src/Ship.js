e633e1080bba9121f82d3d8edbea213b1e1bae2e
dylstar135 <dylstar160990@googlemail.com>
2024-01-19 14:44:05 +0000

    ships now have itinerarys and sail from port to port, and cant go further than last port

--- src/Ship.js
+++ src/Ship.js
@@ -1,15 +1,29 @@
+const Port = require("./Port");
+const Itinerary = require("./Itinerary");
+
 class Ship {
-  constructor(name, startingPort) {
+  constructor(name, itinerary) {
     this.name = name;
-    this.startinPort = startingPort;
+    this.itinerary = itinerary;
+    this.currentPort = itinerary.ports[0];
+    this.previousPort = null;
   }
 
   setSail() {
-    this.startinPort = null;
-    return `${this.name} is a sea`;
+    const itinerary = this.itinerary;
+    const currentPortIndex = itinerary.ports.indexOf(this.currentPort);
+    if (currentPortIndex === this.itinerary.ports.length - 1) {
+      throw new Error("End of Itinerary reached");
+    }
+    return `${this.name} is at sea`;
+  }
+
+  dock() {
+    const itinerary = this.itinerary;
+    const previousPortIndex = itinerary.ports.indexOf(this.previousPort);
+
+    this.currentPort = itinerary.ports[previousPortIndex + 1];
   }
 }
-
-module.exports = Ship;
 
 module.exports = Ship;
